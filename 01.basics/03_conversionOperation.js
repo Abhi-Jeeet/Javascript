9145229d48cac188a9fc89976e865411af20da0d
Abhi-Jeeet <157899302+Abhi-Jeeet@users.noreply.github.com>
2024-04-07 13:51:59 +0000

    Conversion

---
+++ 01.basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score=33
+//console.log(typeof(score));
+
+//converting to string
+
+let numberToString=String(score);//first letter i.e S should be capital
+//console.log(typeof(numberToString));
+//console.log(numberToString);
+
+let score1="33";
+//console.log(typeof(score1));
+//converting to number
+
+let stringToNumber=Number(score1);//first letter i.e N should be capital
+//console.log(typeof(stringToNumber));
+//console.log(stringToNumber);
+
+let naam="33abc"
+//console.log(typeof(naam));
+let naamkanumber=Number(naam);
+//console.log(typeof(naamkanumber));
+//console.log(naamkanumber);
+
+/*
+"33"-> 33
+"33abc"-> NaN(Not a number)
+true=>1; false->0
+*/
+//converting to Boolean
+
+let isLoggedIn="abhijeet"
+
+let booleanloogedin=Boolean(isLoggedIn);
+console.log(typeof(isLoggedIn));
+console.log(booleanloogedin);
+
+//1->true, 0->false
+//""->false
+//"abhijeet"->true
