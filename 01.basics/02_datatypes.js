0f88bcf046ec8690a18ffc2377491c8e6e65f95b
Abhi-Jeeet <157899302+Abhi-Jeeet@users.noreply.github.com>
2024-04-07 13:17:22 +0000

    DataTypes

---
+++ 01.basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"//treat all js code as newer version
+
+let name="abhijeet"//string
+let age=18//number
+
+/*
+number=> digit
+bigint
+string=> " "
+boolean=> true/false
+null=> standalone value
+undefined
+symbol=>unique
+*/
+
+
+//To check the datatype we use (typeof) function.
+console.log(typeof undefined); //output-> datatype-> undefined
+console.log(typeof null);// output-> object
