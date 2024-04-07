fae53ec65f66abae802339b175f2e91897675415
Abhi-Jeeet <157899302+Abhi-Jeeet@users.noreply.github.com>
2024-04-07 10:22:14 +0000

    Done

---
+++ 01.basics/01_variables.js
@@ -0,0 +1,21 @@
+//console.log("Abhijeet Kumar");
+const accountId="4562"; //const represent here a constant 
+let emialId='abhijeet@abhi.com'
+var password="22103" // not use var-> because of issue in block scope and functional scope
+accountCity="chatra"
+let accountMember;
+
+/*accountId='456'
+console.log(accountId);//show error because this is constant will not change*/
+
+emialId="Abhijeetkumar3245@gmail.com"
+console.log(emialId);
+
+password="456123"
+accountCity="Ranchi"
+console.table([password,accountCity])
+
+//ways to printing all the output at a time
+
+console.table([accountId,emialId,password,accountCity,accountMember])
+
